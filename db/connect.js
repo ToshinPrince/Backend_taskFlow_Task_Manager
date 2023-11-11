bbfdd6bc41fb770287d24a33e6728f527b55d6aa
Toshin Prince <toshinprince@gmail.com>
2023-11-11 13:27:44 +0530

    making connection to mongoDB database

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+//only passing second argument as an object to get rid of deprication warnings
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
